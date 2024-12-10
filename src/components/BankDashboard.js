7f5d66f750546c0482975d88aac38c4ddb14c359
AmritRai1 <Amritpal.s.rai@gmail.com>
2024-12-09 21:41:31 -0500

    Add use client directive

--- src/components/BankDashboard.js
+++ src/components/BankDashboard.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import { Search, RefreshCw } from 'lucide-react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
